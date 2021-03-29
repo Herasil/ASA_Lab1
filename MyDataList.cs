925a9da00be8dcc250e92f89e4abcd789eaf631b
Herasil <78405491+Herasil@users.noreply.github.com>
2021-03-29 22:56:11 +0300

    Initial commit

---
+++ MyDataList.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ASA_lab1
+{
+    abstract class DataList<T>
+    {
+        //protected int length;
+        //public int Length { get { return length; } }
+        public abstract double Head();
+        public abstract double Next();
+        //public abstract double getData(int z);
+        public abstract void Swap(int a, double b);
+
+        public T Data;
+        public void Print(int n)
+        {
+            Console.Write(" {0:F5} ", Head());
+            for (int i = 1; i < n; i++)
+                Console.Write(" {0:F5} ", Next());
+            Console.WriteLine();
+        }
+    }
+
+    class MyDataList : DataList<T>
+    {
+       
+        MyLinkedListNode headNode;
+        MyLinkedListNode prevNode;
+        MyLinkedListNode currentNode;
+        private int Count;
+        //constructor
+        public MyDataList()
+        {
+            Count = 0;
+        }
+
+        public void Push(double number)
+        {
+            if (headNode == null)
+                headNode = new MyLinkedListNode(number);
+            else
+                GetLast().nextNode = new MyLinkedListNode(number);
+            Count++;
+
+        }
+
+
+        //get head
+        public override double Head()
+        {
+            currentNode = headNode;
+            prevNode = null;
+            return currentNode.data;
+        }
+
+        //get node by index
+        public override double getData(int z)
+        {
+            if (z == 0)
+                return headNode.data;
+            currentNode = headNode;
+            for (int i = 1; i < Count; i++)
+            {
+                currentNode = currentNode.nextNode;
+                if (z == i)
+                {
+                    return currentNode.data;
+                }
+            }
+            return -1;
+        }
+
+        public override double Next()
+        {
+            prevNode = currentNode;
+            currentNode = currentNode.nextNode;
+            return currentNode.data;
+        }
+       
+        private MyLinkedListNode GetLast()
+        {
+            MyLinkedListNode node = headNode;
+            while(node.nextNode !=  null)
+                node = node.nextNode;
+            return node;
+        }
+        public override void Swap(int a, double b)
+        {
+            Head();
+            for (int i = 0; i < Count; i++)
+            {
+                if (a == i)
+                {
+                    currentNode.data = b;
+                    break;
+                }
+                Next();
+
+            }
+        }
+
+
+        public int getCount()
+        {
+            return Count;
+        }
+
+
+
+
+        public void printAllData()
+        {
+            if(headNode != null) { 
+            MyLinkedListNode node = headNode;
+            Console.Write(node.data + " -> ");
+            while (node.nextNode != null)
+            {
+               
+                node = node.nextNode;
+                    Console.Write(node.data + " -> ");
+                }
+            }
+            Console.Write("null");
+        }
+
+        class MyLinkedListNode
+        {
+            public MyLinkedListNode nextNode { get; set; }
+            public double data { get; set; }
+            public MyLinkedListNode(double data)
+            {
+                this.data = data;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+namespace ASA_lab1
+{
+    class Program
+    {
+        const int min = 10;
+        const int max = 1000;
+        const int numbersToGenerate = 10;
+        const int numberOfBuckets = 11;
+        static Random rand = new Random();
+        static void Main(string[] args)
+        {
+            MyDataList[] bucketSort = new MyDataList[numberOfBuckets];
+            //MyDataList<MyDataList>
+            double[] numbers = new double[numbersToGenerate];
+            double[] numbersBucket = new double[numberOfBuckets];
+            generateDouble(ref numbers);
+            MyDataList[] doubleNumbers = new MyDataList[numberOfBuckets];
+            for (int i = 0; i < numberOfBuckets; i++)
+                doubleNumbers[i] = new MyDataList();
+
+
+
+            Console.WriteLine("Linked List spausdinimai: ");
+            addNumbersToLinkedList(ref doubleNumbers, numbers);
+            printLinkedList(doubleNumbers);
+            InsertionSort(ref doubleNumbers);
+            printLinkedList(doubleNumbers);
+
+
+
+            Console.WriteLine("Array spausdinimai: ");
+            printArray(numbers);
+            Console.WriteLine("");
+            addNumbersToBucket(ref numbersBucket, numbers);
+            //InsertionSort(ref numbersBucket);
+            //printArray(numbers);
+            //InsertionSort(numbers[]);
+        }
+
+
+        public static void printLinkedList(MyDataList[] doubleNumbers)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < numberOfBuckets; i++)
+            {
+                Console.Write(i + " : ");
+                doubleNumbers[i].printAllData();
+                Console.Write("    length: " + doubleNumbers[i].getCount());
+                Console.WriteLine();
+
+            }
+        }
+
+        public static void printArray(double[] numbers)
+        {
+            Console.WriteLine();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+
+        public static void InsertionSort(ref MyDataList[] doubleLinkedList)
+        {
+            for (int z = 0; z < numberOfBuckets; z++)
+            {
+                MyDataList items = doubleLinkedList[z];
+                double currentdata;
+                int n = items.getCount();
+                for (int i = 1; i < n; ++i)
+                {
+                    currentdata = items.getData(i);
+                    int j = i - 1;
+
+
+                    while (j >= 0 && items.getData(j) > currentdata)
+                    {
+                        items.Swap(j + 1, items.getData(j));
+                        j = j - 1;
+                    }
+                    items.Swap(j + 1, currentdata);
+                }
+            }
+
+        }
+
+        //public static void InsertionSortArray(ref double[] numbers)
+        //{
+        //    for (int z = 0; z < numberOfLinkedLists; z++)
+        //    {
+        //        MyDataList items = doubleLinkedList[z];
+        //        double currentdata;
+        //        int n = items.getCount();
+        //        for (int i = 1; i < n; ++i)
+        //        {
+        //            currentdata = items.getData(i);
+        //            int j = i - 1;
+
+
+        //            while (j >= 0 && items.getData(j) > currentdata)
+        //            {
+        //                items.Swap(j + 1, items.getData(j));
+        //                j = j - 1;
+        //            }
+        //            items.Swap(j + 1, currentdata);
+        //        }
+        //    }
+
+        //}
+
+        public static void addNumbersToLinkedList(ref MyDataList[] doubleNumbers, double[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                double currentNumberInFloat = convertNumbersToDouble(numbers[i]);
+
+                Console.Write(currentNumberInFloat + " ");
+
+                for (int j = 0; j < numberOfBuckets; j++)
+                {
+                    int firstNumber = getFirstNumber(currentNumberInFloat);
+                    if (firstNumber == j)
+                    {
+                        doubleNumbers[j].Push(currentNumberInFloat);
+                        break;
+                    }
+                }
+            }
+        }
+
+        public static void addNumbersToBucket(ref double[] numbersBucket, double[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                double currentNumberInFloat = convertNumbersToDouble(numbers[i]);
+
+                Console.Write(currentNumberInFloat + " ");
+
+                for (int j = 0; j < numberOfBuckets; j++)
+                {
+                    int firstNumber = getFirstNumber(currentNumberInFloat);
+                    if (firstNumber == j)
+                    {
+                        numbersBucket[j]=(currentNumberInFloat);
+                        break;
+                    }
+                }
+            }
+        }
+
+
+        public static void generateDouble(ref double[] numArray)
+        {
+            for (int i = 0; i < numbersToGenerate; i++)
+            {
+                numArray[i] = (rand.NextDouble() * (max - min) + min);
+            }
+        }
+
+        public static double convertNumbersToDouble(double numberToConvert)
+        {
+            return Math.Round(((numberToConvert - min) / (max - min)), 2);
+
+        }
+
+        public static int getFirstNumber(double number)
+        {
+            return (int)(number * 10);
+        }
+    }
+}
